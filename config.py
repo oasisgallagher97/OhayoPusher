6fdea4ece444eaeed1d89cd9abd6136f8296f8df
oasisgallagher <oasisgallagher97@gmail.com>
2022-11-30 22:28:02 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# 公众号配置
+# 公众号appId
+app_id = "wxdac2888fdd304cb1"
+# 公众号appSecret
+app_secret = "fcf7701dc443e7b942a1d8f8f4dca86e"
+# 模板消息id
+template_id = "_X8xjS7U7buhTRx5ei6rcXEzj5OBa-nNm0BJkOWSUZc"
+# 接收公众号消息的微信号
+#这是我的
+user = "oawm55mtDVlv54lW1vh-7PGZC8iQ"
+#这是宝宝的
+# user = ""
+
+# 信息配置
+# 所在省份
+province = "江苏"
+# 所在城市
+city = "南京"
+# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+birthday = "1998-6-7"
+# 在一起的日子，格式同上
+love_date = "2022-10-1"
